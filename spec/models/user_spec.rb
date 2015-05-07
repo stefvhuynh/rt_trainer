423c0b830500b5832a1b59fc631d15003a3a6528
Stefan Huynh <huynh.stefan@gmail.com>
2015-05-07 15:38:39 -0600

    Add UsersController

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
+RSpec.describe User, type: :model do
   subject(:user) { FactoryGirl.build(:user) }
 
   let(:secure_random_string) { 'somereallylongstring' }
