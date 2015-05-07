8cce064e23a37d3c5cce3dc1448c619f0048a0da
Stefan Huynh <huynh.stefan@gmail.com>
2015-05-07 15:43:03 -0600

    Move UsersController to api

---
+++ spec/controllers/api/users_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Api::UsersController, type: :controller do
+  subject(:users_controller) { Api::UsersController.new }
+
+  describe '#create' do
+    it ''
+  end
+end
