3956cd264748c57dcc64bda3c41535b174719957
Stefan Huynh <huynh.stefan@gmail.com>
2015-05-19 14:09:36 -0400

    Add some comments to AuthenticationHelper

--- spec/support/authentication_helpers.rb
+++ spec/support/authentication_helpers.rb
@@ -1,4 +1,9 @@
+# The logic for logging in and out are in ApplicationController. It uses the
+# User model and a header called `X-Session-Token`. Here, we mock the log in and
+# log out process so that we don't rely on the User model and we don't hit the
+# database every time.
 module AuthenticationHelpers
+  # `@current_user` becomes nil.
   def mock_log_out
     request.headers['X-Session-Token'] = nil
     allow(User).to receive(:find_by)
@@ -6,7 +11,7 @@ module AuthenticationHelpers
       .and_return(nil)
   end
 
-  # The argument `user` becomes the value of `current_user`.
+  # The argument `user` becomes the value of `@current_user`.
   def mock_log_in(user)
     request.headers['X-Session-Token'] = 'somesessiontoken'
     allow(User).to receive(:find_by)
